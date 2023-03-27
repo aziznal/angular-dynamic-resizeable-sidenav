c0100da28bf0627f33bbcbeb51cafc106ec3a1a6
Aziz <aziznal.dev@gmail.com>
2023-03-28 01:00:48 +0300

    Add ability to resize sidenav

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, HostListener } from '@angular/core';
+
 import { SidenavService } from './sidenav.service';
 
 @Component({
@@ -7,5 +8,47 @@ import { SidenavService } from './sidenav.service';
   styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent {
+  resizingEvent = {
+    // whether the user is currently resizing the sidenav
+    isResizing: false,
+
+    // the x coordinate of the mouse when the user started resizing
+    startingCursorX: 0,
+
+    // the width of the sidenav when the user started resizing
+    startingWidth: 0,
+  };
+
   constructor(public sidenavService: SidenavService) {}
+
+  @HostBinding('class.resizing')
+  get isResizing(): boolean {
+    return this.resizingEvent.isResizing;
+  }
+
+  startResizing(event: MouseEvent): void {
+    this.resizingEvent = {
+      isResizing: true,
+      startingCursorX: event.clientX,
+      startingWidth: this.sidenavService.sidenavWidth,
+    };
+  }
+
+  @HostListener('window:mousemove', ['$event'])
+  updateSidenavWidth(event: MouseEvent) {
+    if (!this.resizingEvent.isResizing) {
+      return;
+    }
+
+    const cursorDeltaX = event.clientX - this.resizingEvent.startingCursorX;
+
+    const newWidth = this.resizingEvent.startingWidth + cursorDeltaX;
+
+    this.sidenavService.setSidenavWidth(newWidth);
+  }
+
+  @HostListener('window:mouseup')
+  stopResizing() {
+    this.resizingEvent.isResizing = false;
+  }
 }
--- src/app/components/sidenav/sidenav.service.ts
+++ src/app/components/sidenav/sidenav.service.ts
@@ -3,4 +3,27 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class SidenavService {}
+export class SidenavService {
+  readonly sidenavMinWidth = 250;
+
+  readonly sidenavMaxWidth = window.innerWidth - 300;
+
+  get sidenavWidth(): number {
+    return parseInt(
+      getComputedStyle(document.body).getPropertyValue('--sidenav-width'),
+      10
+    );
+  }
+
+  /**
+   * Sets the width of the sidenav to given number (clamped between a min and a max) as pixels.
+   */
+  setSidenavWidth(width: number) {
+    const clampedWidth = Math.min(
+      Math.max(width, this.sidenavMinWidth),
+      this.sidenavMaxWidth
+    );
+
+    document.body.style.setProperty('--sidenav-width', `${clampedWidth}px`);
+  }
+}
